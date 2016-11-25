6df498940c93aba6a97a8123a9839a817b597c1b
EndBird <mlh27mlh27@yahoo.ca>
2016-11-25 14:03:18 -0500

    Add files via upload

--- friends.c
+++ friends.c
@@ -16,41 +16,41 @@
  *       (don't forget about the null terminator).
  */
 int create_user(const char *name, User **user_ptr_add) {
-    
-    
+
+
     User *new_user = malloc(sizeof(User));
     if (new_user == NULL) {
         perror("malloc");
         exit(1);
     }
     strncpy(new_user->name, name, MAX_NAME); // name has max length MAX_NAME - 1
-    
+
     for (int i = 0; i < MAX_NAME; i++) {
         new_user->profile_pic[i] = '\0';
     }
-    
+
     new_user->first_post = NULL;
     new_user->next = NULL;
     for (int i = 0; i < MAX_FRIENDS; i++) {
         new_user->friends[i] = NULL;
     }
-    
+
     // Add user to list
     User *prev = NULL;
     User *curr = *user_ptr_add;
     while (curr != NULL && strcmp(curr->name, name) != 0) {
         prev = curr;
-        curr = curr->next;
+        curr = curr->next; printf("friends line43\n");
     }
-    
+
     if (*user_ptr_add == NULL) {       // bug fixed 03/04/2016. Now correct on repeat of 1st name
-        *user_ptr_add = new_user;
+        *user_ptr_add = new_user; printf("friends line47\n");
         return 0;
     } else if (curr != NULL) {
         free(new_user);
         return 1;
     } else {
-        prev->next = new_user;
+        prev->next = new_user; printf("friends line53\n");
         return 0;
     }
 }
@@ -60,21 +60,19 @@ int create_user(const char *name, User **user_ptr_add) {
  * Return a pointer to the user with this name in
  * the list starting with head. Return NULL if no such user exists.
  *
- * NOTE: You'll likely need to cast a (const User *) to a (User *)
- * to satisfy the prototype without warnings.
  */
 User *find_user(const char *name, const User *head) {
     /*    const User *curr = head;
      while (curr != NULL && strcmp(name, curr->name) != 0) {
      curr = curr->next;
      }
-     
+
      return (User *)curr;
      */
     while (head != NULL && strcmp(name, head->name) != 0) {
-        head = head->next;
+        head = head->next; printf("line 75 friends \n");
     }
-    
+    printf("line 77 friends %s\n", print_user(head));
     return (User *)head;
 }
 
@@ -97,24 +95,24 @@ char * list_users(const User *curr) {
     }
     char * str_of_users = malloc(sizeof(char)*(str_size+1)); //for the null terminator
     if (str_of_users == NULL) {
-        fprintf(stderr, "fail to make string for list of users\r\n");
+        fprintf(stderr, "fail to make string for list of users\n");
         return NULL;
     }
     //want to use strcpy first to make sure that the memory reserved
     //are freed from any previous values that may be stored there
     strcpy(str_of_users, curr->name);
     curr = curr->next;
-    
-    strcat(str_of_users, "\r\n");
+
+    strcat(str_of_users, "\n");
     while (curr != NULL) {
         curr_name = curr->name;
         strcat(str_of_users, curr_name);
-        strcat(str_of_users, "\r\n");
+        strcat(str_of_users, "\n");
         curr = curr->next;
     }
     strcat(str_of_users, "\0");
     return str_of_users;
-    
+
 }
 
 
@@ -133,19 +131,18 @@ char * list_users(const User *curr) {
  *   - 3 if the same user is passed in twice.
  *   - 4 if at least one user does not exist.
  *
- * Do not modify either user if the result is a failure.
- * NOTE: If multiple errors apply, return the *largest* error code that applies.
+ *
  */
 int make_friends(const char *name1, const char *name2, User *head) {
     User *user1 = find_user(name1, head);
     User *user2 = find_user(name2, head);
-    
+
     if (user1 == NULL || user2 == NULL) {
         return 4;
     } else if (user1 == user2) { // Same user
         return 3;
     }
-    
+    printf("friends line 148\n");
     int i, j;
     for (i = 0; i < MAX_FRIENDS; i++) {
         if (user1->friends[i] == NULL) { // Empty spot
@@ -154,19 +151,19 @@ int make_friends(const char *name1, const char *name2, User *head) {
             return 1;
         }
     }
-    
+    printf("friends line 157\n");
     for (j = 0; j < MAX_FRIENDS; j++) {
         if (user2->friends[j] == NULL) { // Empty spot
             break;
         }
     }
-    
+    printf("friends line 163\n");
     if (i == MAX_FRIENDS || j == MAX_FRIENDS) { // Too many friends.
         return 2;
     }
-    
-    user1->friends[i] = user2;
-    user2->friends[j] = user1;
+    printf("friends line 167\n");
+    user1->friends[i] = user2; printf("friends line 168\n");
+    user2->friends[j] = user1; printf("friends line 169\n");
     return 0;
 }
 
@@ -181,7 +178,7 @@ char * print_post(const Post *post) {
     if (post == NULL) {
         return NULL;
     }
-    
+
     return post->contents;
 }
 
@@ -199,9 +196,9 @@ char * print_user(const User *user) {
         return NULL;
     }
     int name_len = strlen(user->name);
-    int len_of_name_section = strlen("Name: ") + name_len + strlen("\r\n\r\n") +
-    strlen("------------------------------------------\r\n");
-    int len_of_friends_section = strlen("Friends:\r\n");
+    int len_of_name_section = strlen("Name: ") + name_len + strlen("\n\n") +
+    strlen("------------------------------------------\n");
+    int len_of_friends_section = strlen("Friends:\n");
     const User * curr_user = user;
     int i;
     for (i=0; i<MAX_FRIENDS;i++) {
@@ -209,69 +206,69 @@ char * print_user(const User *user) {
             len_of_friends_section = len_of_friends_section +
             strlen((const char*) (curr_user->friends[i]))+1;}
         else {break;}
-        
+
     }
     len_of_friends_section = len_of_friends_section +
-    strlen("------------------------------------------\r\n");
-    int len_of_posts_section = strlen("Posts:\r\n");
+    strlen("------------------------------------------\n");
+    int len_of_posts_section = strlen("Posts:\n");
     Post * post = curr_user->first_post;
     if (post == NULL) {
         len_of_posts_section = len_of_posts_section +
-        strlen("------------------------------------------\r\n");
+        strlen("------------------------------------------\n");
     }
     else {
         while (post != NULL) {
             len_of_posts_section = len_of_posts_section + strlen("From: ") +
-            strlen(post->author) + strlen("\r\n") + strlen("Date: ") +
-            strlen(ctime(post->date)) + strlen("\r\n\r\n") + strlen(post->contents)
-            + strlen("\r\n===\r\n");
+            strlen(post->author) + strlen("\n") + strlen("Date: ") +
+            strlen(ctime(post->date)) + strlen("\n\n") + strlen(post->contents)
+            + strlen("\n===\n");
             post = post->next;
         }
-        len_of_posts_section = len_of_posts_section - strlen("===\r\n")
-        + strlen("------------------------------------------\r\n");
+        len_of_posts_section = len_of_posts_section - strlen("===\n")
+        + strlen("------------------------------------------\n");
     }
     int len_of_profile = len_of_name_section + len_of_friends_section +
     len_of_posts_section;
     char * profile_str = malloc(sizeof(char)*(len_of_profile+1)); //for null terminator
     if (profile_str == NULL) {
-        fprintf(stderr, "malloc error\r\n");
+        fprintf(stderr, "malloc error\n");
         return NULL;
     }
     strcpy(profile_str, "Name: ");
     strcat(profile_str, curr_user->name);
-    strcat(profile_str, "\r\n\r\n");
-    strcat(profile_str, "------------------------------------------\r\n");
-    strcat(profile_str, "Friends:\r\n");
+    strcat(profile_str, "\n\n");
+    strcat(profile_str, "------------------------------------------\n");
+    strcat(profile_str, "Friends:\n");
     for (i=0; i<MAX_FRIENDS;i++) {
         if (curr_user->friends[i] != NULL)
         {strcat(profile_str, (const char*) curr_user->friends[i]);
-            strcat(profile_str, "\r\n");}
+            strcat(profile_str, "\n");}
         else
-        {strcat(profile_str, "------------------------------------------\r\n");
+        {strcat(profile_str, "------------------------------------------\n");
             break;}
     }
-    strcat(profile_str, "Posts:\r\n");
+    strcat(profile_str, "Posts:\n");
     post = curr_user->first_post;
     if (post==NULL) {
-        strcat(profile_str, "------------------------------------------\r\n");
+        strcat(profile_str, "------------------------------------------\n");
         strcat(profile_str, "\0");
     }
     else {
         while(post != NULL) {
             strcat(profile_str, "From: ");
             strcat(profile_str, post->author);
-            strcat(profile_str, "\r\n");
+            strcat(profile_str, "\n");
             strcat(profile_str, "Date: ");
             strcat(profile_str, ctime(post->date));
-            strcat(profile_str, "\r\n");
+            strcat(profile_str, "\n");
             strcat(profile_str, post->contents);
-            strcat(profile_str, "\r\n");
+            strcat(profile_str, "\n");
             if (post->next != NULL) {
-                strcat(profile_str, "===\r\n");}
+                strcat(profile_str, "===\n");}
             post = post->next;
         }
-        
-        strcat(profile_str, "------------------------------------------\r\n");
+
+        strcat(profile_str, "------------------------------------------\n");
     }
     strcat(profile_str, "\0");
     return profile_str;
@@ -286,9 +283,7 @@ char * print_user(const User *user) {
  *
  * Use the 'time' function to store the current time.
  *
- * 'contents' is a pointer to heap-allocated memory - you do not need
- * to allocate more memory to store the contents of the post.
- *
+ *  *
  * Return:
  *   - 0 on success
  *   - 1 if users exist but are not friends
@@ -298,7 +293,7 @@ int make_post(const User *author, User *target, char *contents) {
     if (target == NULL || author == NULL) {
         return 2;
     }
-    
+
     int friends = 0;
     for (int i = 0; i < MAX_FRIENDS && target->friends[i] != NULL; i++) {
         if (strcmp(target->friends[i]->name, author->name) == 0) {
@@ -309,7 +304,7 @@ int make_post(const User *author, User *target, char *contents) {
     if (friends == 0) {
         return 1;
     }
-    
+
     // Create post
     Post *new_post = malloc(sizeof(Post));
     if (new_post == NULL) {
@@ -326,20 +321,6 @@ int make_post(const User *author, User *target, char *contents) {
     time(new_post->date);
     new_post->next = target->first_post;
     target->first_post = new_post;
-    
-    return 0;
-}
-/**
- * check if the two users are already friends.
- * return 1 if true, 0 if false.
- */
-int already_friends(User u1, User u2) {
-    int i;
-    for (i=0;i<MAX_FRIENDS;i++) {
-        if (*(u1.friends+i) != NULL && *(u1.friends+i) != 0) {
-            if (strcmp((**(u1.friends+i)).name, u2.name)==0 )
-            {return 1;}
-        }
-    }
+
     return 0;
 }
